a86b1aef677d4e848317b0f5630bf9af007814a4
AndresGutierrez <c0dingw.dre@gmail.com>
2022-02-08 11:31:25 -0800

    move background to very back. track touch, and create a box wherever user taps

--- Pachinko/GameScene.swift
+++ Pachinko/GameScene.swift
@@ -10,9 +10,22 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        let background  = SKSpriteNode(imageNamed: "background")
-        background.position = CGPoint(x: 512, y: 384)
+        let background          = SKSpriteNode(imageNamed: "background")
+        background.position     = CGPoint(x: 512, y: 384)
+        
+        // Bleand = How to draw. .replace = draw without worrying about alpha values, good for objects without gaps like our background
+        background.blendMode    = .replace
+        
+        //zPosition = where layer is place. -1 = behind everything else
+        background.zPosition    = -1
         addChild(background)
     }
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let location    = touch.location(in: self)
+        let box         = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        box.position    = location
+        addChild(box)
+    }
 }
